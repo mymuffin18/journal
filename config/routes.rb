3109d89f273916e18dfdb0337e4e8a76ca9b6e77
Josh <josh.uy25@gmail.com>
2022-02-16 15:07:29 +0800

    add index route for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :categories, only: [:index, :show, :create, :update, :destroy]
-      resources :tasks, only: [:show, :create, :update, :destroy ]
+      resources :tasks, only: [:index, :show, :create, :update, :destroy ]
       resources :members, only: [:index]
       
     end
